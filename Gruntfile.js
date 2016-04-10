67069b8711a33c350fc3fb3cfc807fd5f770a4a0
David Haynes <dhaynes3@gmu.edu>
2016-04-10 19:18:17 -0400

    Create masonstrap directory + grunt recognition
    - less files customized
    - if you want to make changes, grunt allows you to do so (maybe a readme writeup?)

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
       amelia:{}, cerulean:{}, cosmo:{}, cyborg:{}, darkly:{},
       flatly:{}, journal:{}, lumen:{}, paper:{}, readable:{},
       sandstone:{}, simplex:{}, slate:{}, spacelab:{}, superhero:{},
-      united:{}, yeti:{}, custom:{}
+      united:{}, yeti:{}, masonstrap:{}
     },
     clean: {
       build: {
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
     var compress = compress == undefined ? true : compress;
 
     var isValidTheme = grunt.file.exists(theme, 'variables.less') && grunt.file.exists(theme, 'bootswatch.less');
- 
+
      // cancel the build (without failing) if this directory is not a valid theme
     if (!isValidTheme) {
       return;
@@ -149,11 +149,11 @@ grunt.registerTask('build_scss', 'build a regular theme from scss', function(the
     grunt.config('sass.dist.options.style', 'expanded');
     grunt.config('sass.dist.options.precision', 8);
     grunt.config('sass.dist.options.unix-newlines', true);
- 
+
     grunt.task.run(['concat', 'sass:dist', 'prefix:' + scssDest, 'clean:build',
         compress ? 'compress_scss:' + scssDest + ':' + '<%=builddir%>/' + theme + '/bootstrap.min.css' : 'none']);
   });
-  
+
   grunt.registerTask('prefix', 'autoprefix a generic css', function(fileSrc) {
     grunt.config('autoprefixer.dist.src', fileSrc);
     grunt.task.run('autoprefixer');
@@ -223,7 +223,7 @@ grunt.registerTask('build_scss', 'build a regular theme from scss', function(the
                 .replace(/(\.(?![0-9])([\w\-]+);)/g, '@extend $1')
                 // 7.  replace string literals
                 .replace(/~"(.*)"/g, '#{"$1"}')
-                // 8.  replace interpolation  ${var} > #{$var} 
+                // 8.  replace interpolation  ${var} > #{$var}
                 .replace(/\$\{(.*)\}/g, '#{$$$1}')
                 // 9.  replace spin to adjust-hue (function name diff)
                 .replace(/spin\(/g, 'adjust-hue(')
