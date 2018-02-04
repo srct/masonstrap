a61d26ab021eafba5db3aa1480d378e624f4ba95
David Haynes <dhaynes3@gmu.edu>
2018-02-03 21:22:14 -0500

    Add a console.log easter egg

---
+++ src/js/masonstrap.js
@@ -0,0 +1,7 @@
+function fn () {
+    console.log(`Masonstrap brought to you by Mason SRCT <3
+https://srct.gmu.edu/`);
+
+}
+
+window.addEventListener && window.addEventListener( "load", fn ) || window.attachEvent && window.attachEvent( "load", fn );
